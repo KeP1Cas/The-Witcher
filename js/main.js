c0dc0bded33ef0b84eb0d63a745fa1feb346eb28
Danil Derevyankin <59162751+KeP1Cas@users.noreply.github.com>
2020-02-08 01:08:23 +0400

    add new style and animated

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ var mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 2,
     loop: true,
     navigation: {
-        nextEl: '.arrow',
-        
+        nextEl: '.arrow', 
       },
+  breakpoints: {
+    // when window width is >= 540px
+    540: {
+      slidesPerView: 2
+    },
+}
 });
+
+let menuButton = document.querySelector('.menu-btn');
+let menu = document.querySelector('.header')
+menuButton.addEventListener('click', function(){
+	menuButton.classList.toggle('menu-btn-active');
+	menu.classList.toggle('header-active');
+})
